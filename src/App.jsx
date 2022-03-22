0092f7bc24727cd43f1dd089da8ed35679f359a0
Jordan Mannfeld <jordanmannfeld@gmail.com>
2022-03-22 00:24:55 -0400

    adds .prettierrc

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,28 @@
-import React, { useState } from "react";
-import ReactDom from "react-dom";
-import { MuuriComponent, useDrag } from "muuri-react";
-import { generateItems } from "./utils";
-import "./index.css";
+import React, { useState } from 'react';
+import ReactDom from 'react-dom';
+import { MuuriComponent, useDrag } from 'muuri-react';
+import { generateItems } from './utils';
+import './index.css';
 
 // App.
 const App = () => {
   // Items state.
   const [items] = useState(generateItems());
   // Items to children.
-  const children = items.map(props => <Item key={props.id} {...props} />);
+  const children = items.map((props) => <Item key={props.id} {...props} />);
 
   return (
     <section className="grid-demo">
-      <MuuriComponent dragEnabled dragPlaceholder={{
-        enabled: true,
-        createElement: function (item) {
-          return item.getElement().cloneNode(true);
-        },
-      }}>{children}
+      <MuuriComponent
+        dragEnabled
+        dragPlaceholder={{
+          enabled: true,
+          createElement: function (item) {
+            return item.getElement().cloneNode(true);
+          }
+        }}
+      >
+        {children}
       </MuuriComponent>
     </section>
   );
@@ -29,7 +33,7 @@ const Item = ({ color, width, height, title }) => {
   // Add a shadow when the item is dragging.
   const isDragging = useDrag();
   // Based on isDragging.
-  const shadow = isDragging ? "shadow" : "";
+  const shadow = isDragging ? 'shadow' : '';
   const cardTitle = title;
 
   return (
@@ -39,4 +43,4 @@ const Item = ({ color, width, height, title }) => {
   );
 };
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { ChakraProvider, extendTheme } from '@chakra-ui/react'
-import './index.css'
-import App from './App'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
+import './index.css';
+import App from './App';
 
 // Extend the theme to include custom colors, fonts, etc
 const colors = {
   brand: {
     900: '#1a365d',
     800: '#153e75',
-    700: '#2a69ac',
-  },
-}
+    700: '#2a69ac'
+  }
+};
 
-const theme = extendTheme({ colors })
+const theme = extendTheme({ colors });
 
 ReactDOM.render(
   <React.StrictMode>
@@ -22,4 +22,4 @@ ReactDOM.render(
     </ChakraProvider>
   </React.StrictMode>,
   document.getElementById('root')
-)
+);
